31171ac137258c7ca539ed620504d8cd9b6252d9
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-11-04 19:46:21 +0900

    aa

---
+++ Practice06/clock_tb.v
@@ -0,0 +1,28 @@
+module clock_tb		;
+
+parameter 		tCK 	= 	1000/50 ; 
+
+reg 			clk 		;
+reg 			rst_n 		;
+
+wire 	[6:0]	o_seg		;
+wire			o_seg_dp 	;
+wire	[5:0]	o_seg_enb	;
+
+initial 		clk = 1'b0	;
+always #(tCK/2) clk = ~clk	;
+
+top_nco_cnt_disp	u0(
+					.o_seg_enb		(	o_seg_enb	),
+					.o_seg_dp		(	o_seg_dp	),
+					.o_seg			(	o_seg		),
+					.clk			(	clk			),
+					.rst_n			(	rst_n		));
+
+initial begin
+		#(0*tCK) rst_n = 1'b0;
+		#(1*tCK) rst_n = 1'b1;
+		#(100000000*tCK) $finish;
+end
+
+endmodule
